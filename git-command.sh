1203923bcb640e417ec3cbefd1982ceca84cbee9
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2024-07-12 18:53:36 +0900

    ✨ ghに対応し関数化

--- git-command.sh
+++ git-command.sh
@@ -1,12 +1,19 @@
 #/bin/bash
-repository_name=$(basename "$PWD")
-user_name=$(git config --global user.name)
+function gitnewrepo() {
+    flag=$1
+    if [[ "$flag" != "--public" && "$flag" != "--private" ]]; then
+        echo "Error: The first argument must be either --public or --private."
+        return 1
+    fi
 
-gh repo create $repository_name
-git init
-echo "# "$repository_name >> README.md
-git add README.md
-git commit -m ":tada: first commit"
-git branch -M main
-git remote add origin git@github.com:$user_name/$repository_name.git
-git push -u origin main
+    repository_name=$(basename "$PWD")
+    user_name=$(git config --global user.name)
+    gh repo create $repository_name $flag
+    git init
+    echo "# "$repository_name >> README.md
+    git add README.md
+    git commit -m ":tada: first commit"
+    git branch -M main
+    git remote add origin git@github.com:$user_name/$repository_name.git
+    git push -u origin main
+}
